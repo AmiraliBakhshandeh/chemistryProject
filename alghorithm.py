f30722ed224d049cb975f369a709444bb315b838
Amirali Bakhshandeh <amiralibakhshandeh86@gmail.com>
2023-01-12 18:02:32 +0330

    fifth

--- alghorithm.py
+++ alghorithm.py
@@ -29,3 +29,86 @@ def Bohr(electron):
         else:
             current_layer += 1
     return layer
+
+
+
+def Circle(draw,list):
+    def draw_atom(pen, radius, levels, electrons_per_level, electron_size=15, electron_color="red"):
+        full_circle = 360
+        for level in range(levels):
+            r = radius * (level + 1)                           # concentric circles
+            electrons_in_level = electrons_per_level[level]
+            arcs = full_circle / electrons_in_level            # arcs between each electron
+            circumference = 0
+            pen_jump(pen, 0, -r)                               # centering
+            while circumference < full_circle:
+                pen.dot(electron_size, electron_color)
+                pen.circle(r, arcs)
+                circumference = circumference + arcs
+    electrons_per_level = []
+    levels = len(list)
+    first_level_radius = 45
+
+    for i in range(levels):
+        e = list[i]
+        electrons_per_level.append(e)
+
+    
+    def pen_jump(pen, x, y):
+        pen.penup()
+        pen.goto(x, y)
+        pen.pendown()
+
+    def turtle_pen(shape="turtle", speed=10, size=3, colors=("blue", "green")):
+        
+        
+        draw.shape(shape)
+        draw.speed(speed)
+        draw.pensize(size)
+        draw.color(*colors)
+        pen_jump(draw, 0, 0)
+        draw.stamp()
+        return draw
+    def turtle_teardown(pen):
+        pen.hideturtle()
+    pen = turtle_pen()
+    draw_atom(pen, first_level_radius, levels, electrons_per_level)
+    turtle_teardown(pen)
+
+def IonCompound(sx,sy,x,y):
+
+    xtest = False
+    ytest = False
+    if x > 4:
+        b1 = 8 - x
+        xd = str(sx) + str(b1)+"-"
+    elif x < 4:
+        b1 = x
+        xd = str(sx) + str(b1) + "+"
+        xtest = True
+    else:
+        return False
+    if y > 4:
+        b2 = 8 - y
+        yd = str(sy) + str(b2) + "-"
+    elif y < 4:
+        b2 = y
+        yd = str(sy) + str(b2) + "+"
+        ytest = True
+    else:
+        return False
+
+    if ytest == True and xtest == True:
+        return False
+    if ytest == False and xtest == False:
+        return False
+
+    if b1 == b2:
+        x1 = sx+sy
+    else:
+        x1 = sx+str(b2)+sy+str(b1)
+    return {
+        "x" : xd,
+        "y" : yd,
+        "result" : x1
+    }
--- app.py
+++ app.py
@@ -1,27 +1,63 @@
 from tkinter import * 
 from tkinter.ttk import *
+from tkinter import messagebox
 import periodic_elems
 import alghorithm
+import turtle
+import json
+
+
 
 
 
 def findit():
+    messagebox.showinfo("راهنما", "با سلام استاد شاهی عزیز . این نرم افزار توسط امیرعلی بخشنده و پندار احمدی 903 ساخته شده است. این نرم افزار توانایی نشان دادن تمامی عناصر جدول تناوبی را در مدل بور و نشان دادن پیوند  یونی بین دو عنصر را دارد که روز ها برای آن زحمت کشیده شده است . امیدواریم از آن لذت ببرید")
+
     val1 = e1.get().capitalize()
     val2 = e2.get().capitalize()
-    
-    
+    draw = turtle.RawTurtle(canvas)
+    draw2 = turtle.RawTurtle(canvas2)
+    list1 = "alghorithm.Bohr(periodic_elems.Element()."+val1+")"
+    list2 = "alghorithm.Bohr(periodic_elems.Element()."+val2+")"
+    bohr1 = eval(list1)
+    bohr2 = eval(list2)
+    ionCompound = alghorithm.IonCompound(val1, val2, bohr1[-1], bohr2[-1])
 
-    execute = f"l3.config(text='{val1} : ' + str(alghorithm.Bohr(periodic_elems.Element().{val1})))"
-    execute2 = f"l4.config(text='{val2} : ' + str(alghorithm.Bohr(periodic_elems.Element().{val2})))"
+
+
+    
     s1 = ""
-    list1 = "s1 = str(alghorithm.Bohr(periodic_elems.Element()."+val1+"))"
-    exec(list1)
-    print(s1)
-    exec(execute) 
-    exec(execute2) 
-
+    s2 = ""
+    list3 = []
+    list4 = []
 
     
+    
+    for x in bohr1 :
+        if x != 0:
+            s1+=str(x)+") "
+            list3.append(x)
+    for x in bohr2 :
+        if x != 0:
+            s2+=str(x)+") "
+            list4.append(x)
+
+
+
+    l3.config(text=f"{val1} : "+ s1)
+    l4.config(text=f"{val2} : "+ s2)
+    l6.config(text=' تناوب :'+str(len(list3))+ ' گروه : ' + str(list3[-1]))
+    l7.config(text=' تناوب :'+str(len(list4))+ ' گروه : ' + str(list4[-1]))
+    l8.config(text=val1)
+    l9.config(text=val2)
+    if ionCompound == False:
+            l10.config(text=f'ترکیب یونی : ترکیب نمیشود')
+    else:       
+        l10.config(text=f'ترکیب یونی : {ionCompound["x"]},{ionCompound["y"]} => {ionCompound["result"]}')
+
+    alghorithm.Circle(draw, list3)
+    alghorithm.Circle(draw2, list4)
+
 
 master = Tk()
 
@@ -30,19 +66,32 @@ e2 = Entry(master)
 button = Button(text='بدست آوردن اطلاعات عنصرها',command=lambda:findit())
 l1 = Label(master, text = "اولین عنصر:")
 l2 = Label(master, text = "دومین عنصر:")
+l5 = Label(master, text="اطلاعات عناصر:")
 l3 = Label()
 l4 = Label()
-l5 = Label(master)
-
-
+l6 = Label()
+l7 = Label()
+l8 = Label()
+l9 = Label()
+l10 = Label()
+canvas = Canvas(master, width = 200, height = 200)
+canvas2 = Canvas(master, width = 200, height = 200)
 
 l1.grid(row = 0, column = 0, sticky = W, pady = 2)
 l2.grid(row = 1, column = 0, sticky = W, pady = 2)
 e1.grid(row = 0, column = 1, pady = 2)
 e2.grid(row = 1, column = 1, pady = 2)
-button.grid(row = 2, column = 0, pady = 2)
-l3.grid(row = 3, column = 0, pady = 2)
-l4.grid(row = 4, column = 0, pady = 2)
-l5.grid(row = 5,column=0,pady = 2)
+button.grid(row = 2, column = 0, pady = 1)
+l3.grid(row = 4, column = 0, pady = 2)
+l4.grid(row = 5, column = 0, pady = 2)
+l5.grid(row = 3,column=0,pady = 2)
+l6.grid(row = 4,column=1,pady = 2)
+l7.grid(row = 5,column=1,pady = 2)
+l8.grid(row = 7,column=0,pady = 2)
+l9.grid(row = 7,column=1,pady = 2)
+l10.grid(row = 6,column=0,pady=2)
+canvas.grid(row=8, column=0,pady=2) 
+canvas2.grid (row=8, column=1) 
 
-mainloop()
+
+master.mainloop()
