de5a50b65d4d53f3f0a603c4742c23593c547f6b
Pendar <ahmadipendar@gmail.com>
2023-01-12 13:24:17 +0330

    command

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from tkinter import *
 from tkinter import ttk
 import json
 
+
+def first()
+    return 0
+def second():
+    return 0
+def third():
+    return 0
+
 tk = Tk()
 command = {
-	
+    "رسوب":first,
+    "دما نزولی":second,
+    "دما صعودی":third 
 }
 
 tk.title("Chemistry Project")
@@ -14,9 +24,9 @@ tk.title("Chemistry Project")
 label = Label(tk,text="لطفا یکی از شرایط زیر را انتخاب بکنید")
 label.pack()
 
-combo = ttk.Combobox(width=100,values=["دما صعودی"," دما نزولی","رسوب"])
+combo = ttk.Combobox(width=100,values=["دما صعودی","دما نزولی","رسوب"])
 combo.pack()
 
-
+command[combo]()
 
 tk.mainloop()
